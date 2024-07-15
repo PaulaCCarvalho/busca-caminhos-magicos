1196ef45c867c94207f8a6b6c69a000910cba71b
Paula Carvalho <paulinhasgst@gmail.com>
2024-07-15 19:51:02 -0300

    Removendo código desnecessário

--- headers/dijkstra.h
+++ headers/dijkstra.h
@@ -4,7 +4,6 @@
 #include "graph.h"
 #include "heap.h"
 
-void inicializaDijkstra(int numDeVertices, int k);
 void encontraKMenoresCaminhos(int numDeVertices, int k, Grafo* grafo, Heap* heap, char* arquivoSaida);
 void imprimeKMenoresCaminhos(int numDeVertices, int k, char* arquivoSaida);
 #endif //BUSCA_K_CAMINHOS_DIJKSTRA_K_SHORTEST_H
